bb38de43daf947c6812a6c929684498aedc2f4fc
narro-tibi <t.zovko@outlook.com>
2022-11-05 13:41:41 +0100

    add Object Pool and Spawner scripts for ingredients

---
+++ Assets/Scripts/Object Spawners/ObjectPool_Ingredients.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Object Pool for multiple ingredients.
+/// They will be initialized and spawned in set order. 
+/// If Pool exceeds initial starting size, new prefabs will be spawned randomly from the Array.
+/// </summary>
+
+public class ObjectPool_Ingredients : MonoBehaviour
+{
+    #region Variables
+
+    // initial Pool objects and size
+    [SerializeField] private GameObject[] ingredientPrefabs;
+    [SerializeField] private Queue<GameObject> ingredientPool = new Queue<GameObject>();
+    [SerializeField] private int poolStartSize = 5;
+
+    #endregion
+
+    #region Functions
+
+    private void Start()
+    {
+        for (int i = 0; i < poolStartSize; i++)
+        {
+            foreach (GameObject GO_ingredient in ingredientPrefabs)
+            {
+                GameObject ingredient = Instantiate(GO_ingredient);
+                ingredientPool.Enqueue(ingredient);
+                ingredient.SetActive(false);
+            }
+        }
+    }
+
+    public GameObject GetIngredient()
+    {
+        if (ingredientPool.Count > 0)
+        {
+            GameObject ingredient = ingredientPool.Dequeue();
+            ingredient.SetActive(true);
+            return ingredient;
+        }
+        else
+        {
+            // if Pool grows beyond initial pool size, add randomization as to which object to spawn
+            GameObject ingredient = Instantiate(ingredientPrefabs[Random.Range(0, ingredientPrefabs.Length)]);
+            return ingredient;
+        }
+    }
+
+    public void ReturnIngredientToPool(GameObject ingredient)
+    {
+        ingredientPool.Enqueue(ingredient);
+        ingredient.SetActive(false);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Object Spawners/Spawner_Ingredients.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/// <summary>
+/// Logic for spawning Ingredient GameObjects from the Object Pool.
+/// They will float along a path until despawning.
+/// </summary>
+
+public class Spawner_Ingredients : MonoBehaviour
+{
+    [SerializeField] private float timeToSpawn = 5f;
+    private float timeSinceSpawn;
+    private ObjectPool_Ingredients objectPool;
+
+    [Tooltip("Starting Spawn Position for ingredients")]
+    [SerializeField] private Vector3 _ingredientSpawnPosition = new Vector3(-1.2f, 2f, 4.5f);
+
+    void Start()
+    {
+        objectPool = FindObjectOfType<ObjectPool_Ingredients>();
+    }
+
+    void FixedUpdate()
+    {
+        timeSinceSpawn += Time.deltaTime;
+        if (timeSinceSpawn >= timeToSpawn)
+        {
+            GameObject newCritter = objectPool.GetIngredient();
+            newCritter.transform.position = _ingredientSpawnPosition;
+            timeSinceSpawn = 0f;
+        }
+    }
+}
